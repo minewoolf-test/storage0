682775be5ad377216f77b7c02cc58ab1a8326a23
Student <Student@DESKTOP-6DRJCKP>
2025-10-29 19:52:29 +0200

    small update

--- storage/Source.cpp
+++ storage/Source.cpp
@@ -16,8 +16,8 @@ int main() {
 	vector <int>volumes = {};
 	vector <int>weights = {};
 	vector <float>proportions = {};
+	int start_volume = 0;
 	vector <string>	storage = {};
-	int cur_volume = 0;
 	int cur_weight = 0;
 
 	//Инициализация и чтение
@@ -32,7 +32,8 @@ int main() {
 
 	//Инициализация объема
 	cout << "Введите объем ";
-	cin >> cur_volume;
+	cin >> start_volume;
+	int cur_volume = start_volume;
 
 	//Подбор
 	while (proportions_copy.size() > 0) {
@@ -55,7 +56,45 @@ int main() {
 		//Сохранение изменений вектора
 		proportions_copy = proportions_copy_new;
 	};
+	//пересмотр объема
+	int remains = cur_volume;
+	cur_volume = start_volume;
+	names = {};
+	volumes = {};
+	weights = {};
+	proportions = {};
+	storage = {};
+	cur_weight = 0;
+	for (int i = 0; i < items.size(); i++) {
+		vector <string>	item = split(items[i], ",");
+		names.push_back(item[0]);
+		volumes.push_back(stoi(item[1]));
+		weights.push_back(stoi(item[2]));
+		proportions.push_back(stof(item[2]) / stof(item[1])+ stof(item[1])*remains/ start_volume);
+	};
+	proportions_copy=proportions;
 
+	//Подбор
+	while (proportions_copy.size() > 0) {
+		//Выбор наиболее выгодного предмета
+		float maximum = *max_element(proportions_copy.begin(), proportions_copy.end());
+		vector <float>proportions_copy_new = {};
+		for (int i = 0; i < proportions_copy.size(); i++) {
+			if (proportions_copy[i] != maximum) {
+				proportions_copy_new.push_back(proportions_copy[i]);
+			};
+		};
+		//Добавление в рюкзак
+		for (int i = 0; i < proportions.size(); i++) {
+			if (proportions[i] == maximum && cur_volume >= volumes[i] && proportions[i] > 0) {
+				storage.push_back(names[i]);
+				cur_weight += weights[i];
+				cur_volume -= volumes[i];
+			};
+		};
+		//Сохранение изменений вектора
+		proportions_copy = proportions_copy_new;
+	};
 	//Вывод
 	cout << "Объекты: " << endl;
 	for (int i = 0; i < storage.size(); i++) {
